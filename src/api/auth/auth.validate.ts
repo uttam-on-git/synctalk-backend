380679a5d150f8d5e57adba3e0853c187de15816
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2025-07-25 20:11:21 +0530

    feat:add zod schema for auth validate

--- src/api/auth/auth.validate.ts
+++ src/api/auth/auth.validate.ts
@@ -1,2 +1,16 @@
 import { z } from "zod"
 
+const registerSchema = z.object({
+  body: z.object({
+    username: z.string().min(3, { error: 'Username must be at least 3 characters long' }),
+    email: z.email({ error: 'Invalid email address' }),
+    password: z.string().min(6, { error: 'Password must be at least 6 characters long' })
+  })
+});
+
+const loginSchema = z.object({
+  body: z.object({
+    email: z.email({ error: 'Invalid email address' }),
+    password: z.string().min(1, { error: 'Password is required' })
+  })
+});
